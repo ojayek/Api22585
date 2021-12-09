0159a3872b763bdcb2a8487cfa5017badeb60017
Nemat Doustinouri <sn22672@gmail.com>
2021-12-09 12:24:14 +0330

    minor changes

--- CreateXmlWebApi/Models/Model.Designer.cs
+++ CreateXmlWebApi/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Moshanir\CreateXmlWebApi\CreateXmlWebApi\Models\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Darman\Api\CreateXmlWebApi\Models\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
